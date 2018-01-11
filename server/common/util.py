5307f8748335d8b62bbd8be98d34f8433a6ed9b3
Leonardo Alves <leunardosevla@gmail.com>
2018-01-11 14:38:09 -0200

    Formatar arquivo

--- server/common/util.py
+++ server/common/util.py
@@ -2,14 +2,13 @@
 
 from datetime import datetime
 
+
 def hora_atual():
     """"Retorna a hora atual"""
     hora = str(datetime.now())
     return hora
 
+
 def send_response(data, status=200, error=None):
     """Wrapper que é responsável por padronizar as respostas feitas pelo servidor"""
-    return { 'data': data, 
-             'status': status, 
-             'error': error  }
-             
+    return {'data': data, 'status': status, 'error': error  }
