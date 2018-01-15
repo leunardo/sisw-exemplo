0be627f9d9ff8e8d71e2836b95d4bd221fae03d8
Leonardo Alves <leunardosevla@gmail.com>
2018-01-15 15:56:36 -0200

    Abstrair make_response do flask

--- server/common/util.py
+++ server/common/util.py
@@ -13,7 +13,11 @@ def hora_atual():
 
 class Response(flask.Response):
 
-    def __init__(self, data, status=200, headers=None):
-        if headers is None: headers = {}
+    def __init__(self, data, status, headers):
         headers['Content-type'] = 'application/json'
-        super().__init__(response=dumps(data), status=200, headers=headers)
+        super().__init__(response=dumps(data), status=status, headers=headers)
+
+
+def make_response(data, status=200, headers=None):
+    if headers is None: headers = {}
+    return flask.make_response(Response(data, status, headers))
