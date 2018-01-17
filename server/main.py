4c7ecf76fd3646fb9ad97cc76c06f5f100b4d387
Leonardo Alves <leunardosevla@gmail.com>
2018-01-17 11:18:40 -0200

    Adicionar cors e multi thread em servidor

--- server/main.py
+++ server/main.py
@@ -2,6 +2,7 @@
 
 from flask import Flask
 from flask_restful import Api
+from flask_cors import CORS
 
 # importa nossos recursos
 from resources.hello_world import HelloWorld
@@ -9,8 +10,8 @@ from resources.hour import Hour
 from resources.usuario import UsuarioResource
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
-
 # adicionamos os recursos para as rotas /hello e /hello/<string>
 api.add_resource(HelloWorld, '/hello', '/hello/<string:nome>')
 api.add_resource(Hour, '/hour')
@@ -18,4 +19,4 @@ api.add_resource(UsuarioResource, '/user', '/user/<int:id_usuario>')
 
 if __name__ == '__main__':
     # desativar debug em produção
-    app.run(debug=True)
+    app.run(debug=True, threaded=True)
